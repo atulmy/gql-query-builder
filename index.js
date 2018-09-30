44660336168fab3d4b145019ad49e35c3110af24
atulmy <atul.12788@gmail.com>
2018-09-30 14:17:03 +0530

    Init

---
+++ index.js
@@ -0,0 +1,46 @@
+"use strict";
+
+module.exports = function queryBuilder(options) {
+  options.type = options.type ? options.type : 'query'
+  options.operation = options.operation ? options.operation : ''
+  options.fields = options.fields ? options.fields : []
+  options.data = options.data ? options.data : {}
+  options.variables = options.variables ? options.variables : {}
+
+  return {
+    query: `
+            ${ options.type } ${ queryDataArgumentAndTypeMap(options.data) } {
+                ${ options.operation } ${ queryDataNameAndArgumentMap(options.data) } {
+                    ${ options.fields.join(',') }
+                }
+            }`,
+    variables: Object.assign(options.data, options.variables)
+  }
+}
+
+// Convert object to name and argument map eg: (id: $id)
+function queryDataNameAndArgumentMap(data) {
+  return Object.keys(data).length ? `(${ Object.keys(data).reduce((dataString, key, i) => `${ dataString }${ i !== 0 ? ', ' : '' }${ key }: $${ key }`, '') })` : ''
+}
+
+// Convert object to argument and type map eg: ($id: Int)
+function queryDataArgumentAndTypeMap(data) {
+  return Object.keys(data).length ? `(${ Object.keys(data).reduce((dataString, key, i) => `${ dataString }${ i !== 0 ? ', ' : '' }$${ key }: ${ queryDataType(data[key]) }`, '') })` : ''
+}
+
+// Private - Get GraphQL equivalent type of data passed (String, Int, Float, Boolean)
+function queryDataType(data) {
+  switch (typeof data) {
+    case 'object':
+      const type = data._type
+      delete data._type
+      return type
+    case 'boolean':
+      return 'Boolean'
+    case 'number':
+      return (data % 1 === 0) ? 'Int' : 'Float'
+    case 'string':
+    default:
+      return 'String'
+  }
+}
